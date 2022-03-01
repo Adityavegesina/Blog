aa7d182013c1b49858c3008448c8fb91ff53079b
Aditya_Vegesina <adityavegesina@gmail.com>
2022-03-01 13:43:20 +0530

    adding project

---
+++ About.js
@@ -0,0 +1,11 @@
+import './about.css'
+
+const ABOUTPAGE = () => {
+    return (
+        <div className="about-page">
+            <h1>About<span id="us">Us</span></h1>
+        </div>
+    );
+}
+
+export default ABOUTPAGE;
